0cce868bc0fa64f7f88f3e8ad859b464957ab305
Lukas Honold <honoldlukas@web.de>
2021-02-08 19:20:40 +0100

    add main

--- src/main.cpp
+++ src/main.cpp
@@ -28,68 +28,105 @@
 #include "DisplayManager.h"
 #include "KeypadManager.h"
 #include "LEDManager.h"
-#include "PowerManager.h"
 
 #define relay_pin A3
 #define chip_select_rfid 5
 #define reset_rfid 4
 #define chip_select_sd 6
-#define ledpin 6
+#define led_pin 7
 
-
-
-
-class Hardware {
+class Hardware
+{
 public:
     Hardware(int countdown) : dataManager(chip_select_sd), keypadManager(10),
                               cardReader(chip_select_rfid, reset_rfid, 4), displayManager(0x27, 20, 4, 5),
-                              ct(countdown),powerManager(relay_pin) {};
+                              ct(countdown), ledManager(led_pin, 10){};
     DataManager dataManager;
     KeypadManager keypadManager;
     CardReader cardReader;
     DisplayManager displayManager;
     Countdown ct;
-    PowerManager powerManager;
+    LEDManager ledManager;
 };
 
-class StateMaschine {
+class StateMaschine
+{
 public:
-
-
-    enum class States {
-        Warten,ID_gelesen
+    enum class States
+    {
+        WARTEN,
+        ID_GELESEN,
+        ABGEBUCHT,
+        AUFLADEN,
+        ID_AUSGEGEBEN
     };
 
-    StateMaschine(int countdown_init) : hardware(countdown_init) {};
+    StateMaschine(int countdown_init) : hardware(countdown_init){};
 
-
-    void update() {
+    void update()
+    {
         hardware.cardReader.update();
         hardware.keypadManager.update();
         hardware.displayManager.update();
+        hardware.ledManager.update();
     };
 
-    void run(){
+    void run()
+    {
         update();
-        switch (current_state) {
-            case States::Warten:
-                break;
-            case States::ID_gelesen:
-                break;
+        switch (current_state)
+        {
+        case States::WARTEN:
+            // Brüßungstext
+            hardware.displayManager.set_new_text("Was machen Sachen?");
+
+            // Karte auslesen und LED an- bzw. ausschalten
+            if (hardware.cardReader.is_card_present())
+            {
+                hardware.ledManager.toggle_permanent(true);
+            }
+            else
+            {
+                hardware.ledManager.toggle_permanent(false);
+            }
+
+            // Zustand setzen
+            current_state = States::ID_GELESEN;
+            // Testkommentar
+            break;
+
+        case States::ID_GELESEN:
+            if (hardware.keypadManager.is_pressed()){
+                if(hardware.keypadManager.get_key() == '*'){
+                    hardware.displayManager.set_new_text(String(hardware.keypadManager.get_key()));
+                    current_state = States::ID_AUSGEGEBEN;
+                }
+            }
+            break;
+        case States::ABGEBUCHT:
+            break;
+        case States::AUFLADEN:
+            break;
+        case States::ID_AUSGEGEBEN:
+            break;
         }
     };
 
 private:
-    States current_state=States::Warten;
+    States current_state = States::WARTEN;
     Hardware hardware;
 };
 
-void setup() {
+void setup()
+{
     Serial.begin(9600);
+    pinMode(relay_pin, OUTPUT);
+    digitalWrite(relay_pin, LOW);
     StateMaschine stm(20);
     stm.run();
 }
 
-void loop() {
+void loop()
+{
     digitalWrite(relay_pin, HIGH); // Selbsthaltung ausschalten
 }
