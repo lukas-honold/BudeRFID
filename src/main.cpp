59903511af8f91ad0f446a03d8c04dad8c0dee8a
michael_kirsch <michael_kirsch@t-online.de>
2021-02-08 19:23:38 +0100

    add power and countdownloop to statemaschine

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,7 @@
 #include "DisplayManager.h"
 #include "KeypadManager.h"
 #include "LEDManager.h"
+#include "PowerManager.h"
 
 #define relay_pin A3
 #define chip_select_rfid 5
@@ -40,13 +41,14 @@ class Hardware
 public:
     Hardware(int countdown) : dataManager(chip_select_sd), keypadManager(10),
                               cardReader(chip_select_rfid, reset_rfid, 4), displayManager(0x27, 20, 4, 5),
-                              ct(countdown), ledManager(led_pin, 10){};
+                              ct(countdown), ledManager(led_pin, 10),powerManager(relay_pin){};
     DataManager dataManager;
     KeypadManager keypadManager;
     CardReader cardReader;
     DisplayManager displayManager;
     Countdown ct;
     LEDManager ledManager;
+    PowerManager powerManager;
 };
 
 class StateMaschine
@@ -58,7 +60,8 @@ public:
         ID_GELESEN,
         ABGEBUCHT,
         AUFLADEN,
-        ID_AUSGEGEBEN
+        ID_AUSGEGEBEN,
+        AUSSCHALTEN
     };
 
     StateMaschine(int countdown_init) : hardware(countdown_init){};
@@ -73,43 +76,46 @@ public:
 
     void run()
     {
-        update();
-        switch (current_state)
-        {
-        case States::WARTEN:
-            // Brüßungstext
-            hardware.displayManager.set_new_text("Was machen Sachen?");
-
-            // Karte auslesen und LED an- bzw. ausschalten
-            if (hardware.cardReader.is_card_present())
+        while (hardware.ct.alive()){
+            update();
+            switch (current_state)
             {
-                hardware.ledManager.toggle_permanent(true);
-            }
-            else
-            {
-                hardware.ledManager.toggle_permanent(false);
-            }
+                case States::WARTEN:
+                    // Brüßungstext
+                    hardware.displayManager.set_new_text("Was machen Sachen?");
 
-            // Zustand setzen
-            current_state = States::ID_GELESEN;
-            // Testkommentar
-            break;
+                    // Karte auslesen und LED an- bzw. ausschalten
+                    if (hardware.cardReader.is_card_present())
+                    {
+                        hardware.ledManager.toggle_permanent(true);
+                    }
+                    else
+                    {
+                        hardware.ledManager.toggle_permanent(false);
+                    }
 
-        case States::ID_GELESEN:
-            if (hardware.keypadManager.is_pressed()){
-                if(hardware.keypadManager.get_key() == '*'){
-                    hardware.displayManager.set_new_text(String(hardware.keypadManager.get_key()));
-                    current_state = States::ID_AUSGEGEBEN;
-                }
+                    // Zustand setzen
+                    current_state = States::ID_GELESEN;
+                    // Testkommentar
+                    break;
+
+                case States::ID_GELESEN:
+                    if (hardware.keypadManager.is_pressed()){
+                        if(hardware.keypadManager.get_key() == '*'){
+                            hardware.displayManager.set_new_text(String(hardware.keypadManager.get_key()));
+                            current_state = States::ID_AUSGEGEBEN;
+                        }
+                    }
+                    break;
+                case States::ABGEBUCHT:
+                    break;
+                case States::AUFLADEN:
+                    break;
+                case States::ID_AUSGEGEBEN:
+                    break;
             }
-            break;
-        case States::ABGEBUCHT:
-            break;
-        case States::AUFLADEN:
-            break;
-        case States::ID_AUSGEGEBEN:
-            break;
         }
+        hardware.powerManager.switch_off();
     };
 
 private:
