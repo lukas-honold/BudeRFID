4c4ca1841cd37ea9a34c9edb20c877f14ff6f89a
michael_kirsch <michael_kirsch@t-online.de>
2021-02-08 17:42:20 +0100

    add statemaschine

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
+
 #define arduino_ide
 
 #ifdef arduino_ide
+
 #include <Keypad.h>
 #include <LiquidCrystal_I2C.h>
 #include <MFRC522.h>
@@ -18,13 +20,6 @@
 
 #endif
 
-
-#define relay_pin A3
-#define led_pin 7
-#define chip_select_rfid 5
-#define reset_rfid 4
-#define chip_select_sd 6
-
 #include "Timer.h"
 #include "Countdown.h"
 #include "Person.h"
@@ -33,34 +28,109 @@
 #include "DisplayManager.h"
 #include "KeypadManager.h"
 
+#define relay_pin A3
+#define led_pin 7
+#define chip_select_rfid 5
+#define reset_rfid 4
+#define chip_select_sd 6
+
+
+class Hardware{
+public:
+    Hardware(int countdown): dataManager(chip_select_sd), keypadManager(10),
+                             cardReader(chip_select_rfid, reset_rfid, 4), displayManager(0x27, 20, 4, 5),
+                             ct(countdown){};
+    DataManager dataManager;
+    KeypadManager keypadManager;
+    CardReader cardReader;
+    DisplayManager displayManager;
+    Countdown ct;
+};
+
+
+class State;
+class StateMaschine{
+public:
+    void set_next_state(State*);
+};
+class TestState;
+class Test2State;
+
+
+class State {
+public:
+    State(StateMaschine *stm,Hardware& hrdw):m_hardware(hrdw),m_stm(stm) {
+
+    }
+
+    virtual void update() = 0;
+
+    StateMaschine* m_stm;
+    Hardware& m_hardware;
+};
+
+
+class TestState : private State{
+public:
+    TestState(StateMaschine*stm,Hardware& hrdw):State(stm,hrdw){
+
+    };
+
+    void update(){
+        m_stm->set_next_state(m_stm->test2);
+    }
+
+};
+
+
+
+class StateMaschine {
+public:
+    StateMaschine(int countdown) : hardware(countdown) {
+        hardware.dataManager.import_data();
+    };
+
+    void update() {
+        hardware.keypadManager.update();
+        hardware.displayManager.update();
+        hardware.cardReader.update();
+    }
+
+    void run() {
+        while (hardware.ct.alive())
+        {
+            update();
+            running_state->update();
+        }
+    }
+
+
+
+    void set_next_state(State* next_state){
+        running_state = next_state;
+    }
+
+
+private:
+    Hardware hardware;
+    State* running_state;
+};
+
+
+
+
+
+
+
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Hello");
-  pinMode(relay_pin, OUTPUT);
-  digitalWrite(relay_pin, LOW);
-  DataManager dataManager(chip_select_sd);
-  dataManager.import_data();
-  KeypadManager keypadManager(10);
-  CardReader cardReader(chip_select_rfid, reset_rfid, 4);
-  DisplayManager displayManager(0x27, 20, 4, 5);
-  Countdown ct(20.0);
-
-  while (ct.alive()) {
-    keypadManager.update();
-    cardReader.update();
-    if (keypadManager.get_is_pressed()) {
-      ct.reset();
-      Serial.println(keypadManager.get_key());
-      displayManager.update();
-    }
-    displayManager.set_new_text("ID:" + String(cardReader.get_id()));
-    displayManager.update();
-
-  }
-
+    Serial.begin(9600);
+    pinMode(relay_pin, OUTPUT);
+    digitalWrite(relay_pin, LOW);
+    StateMaschine stm(20);
+    stm.run();
 }
 
 void loop() {
-  digitalWrite(relay_pin, HIGH); // Selbsthaltung ausschalten
+    digitalWrite(relay_pin, HIGH); // Selbsthaltung ausschalten
 }
