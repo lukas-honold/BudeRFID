425da74d7a3d5948d650a69456bdbb79037ab026
michael_kirsch <michael_kirsch@t-online.de>
2021-02-08 20:05:42 +0100

    fix display manager clear function

--- src/DisplayManager.h
+++ src/DisplayManager.h
@@ -12,17 +12,24 @@ class DisplayManager {
 
     void set_new_text(String to_print) {
       next_input = to_print;
+      change_made = true;
     };
 
     void update() {
       if (m_timer.update()) {
-        test += 1;
-        lcd.setCursor(0, 0);
-        lcd.print(next_input);
+          if(change_made){
+              change_made = false;
+              test += 1;
+              lcd.clear();
+              lcd.setCursor(0, 0);
+              lcd.print(next_input);
+          }
+
       }
     };
 
   private:
+    bool change_made = false;
     Timer m_timer;
     String next_input;
     LiquidCrystal_I2C lcd;  // set the LCD address to 0x27 for a 16 chars and 2 line display
