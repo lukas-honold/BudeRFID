838d565ce66f9009cb6e646b06097ea21f42e076
michael_kirsch <michael_kirsch@t-online.de>
2021-02-09 10:40:48 +0100

    new statemaschine with class based structure

--- src/main.cpp
+++ src/main.cpp
@@ -12,34 +12,15 @@
 #include "Timer.h"
 #include "Countdown.h"
 #include "Person.h"
-#include "DataManager.h"
-#include "CardReader.h"
-#include "DisplayManager.h"
-#include "KeypadManager.h"
-#include "LEDManager.h"
-#include "PowerManager.h"
 
-#define relay_pin A3
-#define chip_select_rfid 5
-#define reset_rfid 4
-#define chip_select_sd 6
+#include "Hardware.h"
+#include "Statemaschine.h"
+
+
 #define led_pin 7
 
 
-class Hardware
-{
-public:
-    Hardware(int countdown) : dataManager(chip_select_sd), keypadManager(10),
-                              cardReader(chip_select_rfid, reset_rfid, 4), displayManager(0x27, 20, 4, 5),
-                              ct(countdown), ledManager(led_pin, 10), powerManager(relay_pin){};
-    DataManager dataManager;
-    KeypadManager keypadManager;
-    CardReader cardReader;
-    DisplayManager displayManager;
-    Countdown ct;
-    LEDManager ledManager;
-    PowerManager powerManager;
-};
+/*
 
 class StateMaschine
 {
@@ -165,11 +146,23 @@ private:
     Countdown pause;
 };
 
+*/
+
+
+
 void setup()
 {
+    Hardware hrdw(20);
     Serial.begin(9600);
-    StateMaschine stm(20);
-    stm.run();
+
+    StateMaschine stm(&hrdw);
+
+    State* states[]={
+            new Test1State(&stm),
+            new Test2State(&stm),
+    };
+
+    stm.run(states,2,StateIdentifier::State1);
 }
 
 void loop()
