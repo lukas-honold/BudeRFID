c2e223e5072ab8ca6e92a1237633462fbd444f5e
michael_kirsch <michael_kirsch@t-online.de>
2021-02-09 13:44:42 +0100

    add second line feature for DisplayManager.h

--- src/DisplayManager.h
+++ src/DisplayManager.h
@@ -1,38 +1,54 @@
 
 #pragma once
+
+struct Cursor {
+    int x = 0;
+    int y = 0;
+};
+
 class DisplayManager {
-  public:
+public:
     DisplayManager(int address, int p1, int p2, int i_framerate) : lcd(address, p1, p2), m_timer(i_framerate) {
-      lcd.init();
-      lcd.backlight();
-      lcd.setCursor(3, 0);
+        lcd.init();
+        lcd.backlight();
+        lcd.setCursor(3, 0);
+        cursor_line1.x = 0;
+        cursor_line2.y = 0;
+        cursor_line2.x = 0;
+        cursor_line2.y = 1;
+
     };
 
     ~DisplayManager() = default;
 
-    void set_new_text(String to_print) {
-      next_input = to_print;
-      changes_made = true;
+    void set_new_text(String to_print, bool line = false) {
+        if(line){
+            line2 = to_print;
+        } else{
+            line1 = to_print;
+        }
+        changes_made = true;
     };
+
 
 
     void update() {
-      if (m_timer.update()) {
-          if (changes_made){
-              changes_made = false;
-              lcd.clear();
-              lcd.setCursor(0, 0);
-              lcd.print(next_input);
-          }
-
-
-      }
+        if (m_timer.update()) {
+            if (changes_made) {
+                changes_made = false;
+                lcd.clear();
+                lcd.setCursor(cursor_line1.x,cursor_line1.y);
+                lcd.print(line1);
+                lcd.setCursor(cursor_line2.x,cursor_line2.y);
+                lcd.print(line2);
+            }
+        }
     };
 
-  private:
+private:
+    String line1, line2;
+    Cursor cursor_line1, cursor_line2;
     Timer m_timer;
-    String next_input;
-    String next_input_second_line;
     LiquidCrystal_I2C lcd;  // set the LCD address to 0x27 for a 16 chars and 2 line display
     float frametime = 0.f;
     bool changes_made = false;
