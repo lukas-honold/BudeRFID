bec81dff4b0f2627c9936a896c98f7047c117323
michael_kirsch <michael_kirsch@t-online.de>
2021-02-10 11:58:53 +0100

    revert to basic DisplayManager.h

--- src/DisplayManager.h
+++ src/DisplayManager.h
@@ -6,29 +6,6 @@ struct Cursor {
     int y = 0;
 };
 
-class CustomCharGenerator{
-public:
-    CustomCharGenerator(){
-        byte customChar[8] = {
-                B00000,
-                B00000,
-                B00000,
-                B00000,
-                B00000,
-                B00000,
-                B00000,
-                B00000
-        };
-        byte full = B11111;
-        for(int x=7;x<=0;x--){
-            customChar[x]= full;
-            for(int y=0;y<8;y++)
-                new_chars[x][y]=customChar[y];
-        }
-    }
-    byte new_chars[8][8];
-};
-
 
 
 class DisplayManager {
@@ -41,15 +18,7 @@ public:
         cursor_line2.y = 0;
         cursor_line2.x = 0;
         cursor_line2.y = 1;
-        //CustomCharGenerator gen;
-        //for(int x=0;x<8;x++){
-        //    lcd.createChar(x, gen.new_chars[x]);
-        //}
     };
-    void write_percentage_char(Cursor position,int percent){
-        custom_char_cursor = position;
-        current_custom_char = byte(map(percent,0,100,0,7));//map percentage to respective char
-    }
     ~DisplayManager() = default;
 
     void set_new_text(String to_print, bool line = false) {
@@ -67,9 +36,6 @@ public:
         }
     };
 
-    bool custom_char_gets_drawn = false;
-
-
 private:
     void update_display(){
             if (changes_made) {
@@ -79,10 +45,6 @@ private:
                 lcd.print(line1);
                 lcd.setCursor(cursor_line2.x, cursor_line2.y);
                 lcd.print(line2);
-                if(custom_char_gets_drawn){
-                    lcd.setCursor(custom_char_cursor.x,custom_char_cursor.y);
-                    lcd.write(current_custom_char);
-                }
             }
     };
 
@@ -90,11 +52,8 @@ private:
 
 
     String line1, line2;
-    Cursor cursor_line1, cursor_line2,custom_char_cursor;
-    byte current_custom_char=0;
+    Cursor cursor_line1, cursor_line2;
     Timer m_timer;
     LiquidCrystal_I2C lcd;  // set the LCD address to 0x27 for a 16 chars and 2 line display
-    float frametime = 0.f;
     bool changes_made = false;
-    bool changes_made_second_line = false;
 };
